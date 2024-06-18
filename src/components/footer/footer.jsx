ae008a3ef12e6ad7bc6f8eb4063a30eb2c36444d
Hamid Asadi <hamid.19471@gmail.com>
2024-06-18 12:09:22 +0330

    Create: basic header and footer components

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,4 @@
+const Footer = () => {
+  return <footer>Footer Section</footer>;
+};
+export default Footer;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between w-full">
+      <span>logo</span>
+      <span>login/signup</span>
+    </header>
+  );
+};
+export default Header;
