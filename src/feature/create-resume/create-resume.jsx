1b45c21a989e712cecbd76e38b3cc91830679dd3
Hamid Asadi <hamid.19471@gmail.com>
2024-06-20 14:08:34 +0330

    Update: complete step 1 for create resume for add title and pass some user information to Strapi

--- src/feature/create-resume/create-resume.jsx
+++ src/feature/create-resume/create-resume.jsx
@@ -1,4 +1,4 @@
-import { PlusSquare } from "lucide-react";
+import { Loader2, PlusSquare } from "lucide-react";
 import {
   Dialog,
   DialogContent,
@@ -10,9 +10,42 @@ import {
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@clerk/clerk-react";
+import { httpService } from "@/core/HttpService";
 
 const CreateResume = () => {
   const [openDialog, setOpenDialog] = useState(false);
+  const [resumeTitle, setResumeTitle] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { user } = useUser();
+
+  const handleSubmitTitle = async () => {
+    setLoading(true);
+    const uuid = uuidv4();
+    const data = {
+      data: {
+        title: resumeTitle,
+        resumeId: uuid,
+        userEmail: user?.primaryEmailAddress.emailAddress,
+        userName: user?.fullName,
+      },
+    };
+    try {
+      const res = await httpService.post("/user-resumes", data);
+      if (res) {
+        setOpenDialog(false);
+        setResumeTitle("");
+      }
+    } catch (error) {
+      if (error) {
+        console.log(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <div
@@ -32,6 +65,8 @@ const CreateResume = () => {
             </DialogTitle>
             <DialogDescription>
               <Input
+                value={resumeTitle}
+                onChange={(e) => setResumeTitle(e.target.value)}
                 type="text"
                 placeholder="Ex. Fullstack Developer"
                 className="mt-2"
@@ -47,7 +82,9 @@ const CreateResume = () => {
               >
                 Cancel
               </Button>
-              <Button>Create</Button>
+              <Button onClick={handleSubmitTitle}>
+                {loading ? <Loader2 className="animate-spin" /> : "Create"}
+              </Button>
             </div>
           </DialogFooter>
         </DialogContent>
