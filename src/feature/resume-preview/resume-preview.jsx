83c131a30a76ffa61c1c9c87fe318ddca66f89c0
Hamid Asadi <hamid.19471@gmail.com>
2024-06-21 19:25:39 +0330

    Create: resume preview section

---
+++ src/feature/resume-preview/resume-preview.jsx
@@ -0,0 +1,25 @@
+import { useAppContext } from "@/context/app-context/app-context";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import PersonalInformation from "./components/personal-information";
+import SummeryInformation from "./components/summery-information";
+import ExperienceInformation from "./components/experience-information";
+
+const ResumePreview = () => {
+  const { resumeId } = useParams();
+  const { resumeInfo, ResumeById } = useAppContext();
+  // useEffect(() => {
+  //   resumeId && ResumeById(resumeId);
+  // }, [resumeId]);
+  return (
+    <div
+      className="bg-white w-full p-8 flex flex-col border-t-[20px] "
+      style={{ borderColor: resumeInfo?.themeColor }}
+    >
+      <PersonalInformation resumeInfo={resumeInfo} />
+      <SummeryInformation resumeInfo={resumeInfo} />
+      <ExperienceInformation resumeInfo={resumeInfo} />
+    </div>
+  );
+};
+export default ResumePreview;
