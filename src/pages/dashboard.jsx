cb9f5e9e2a163e521f7c47d153bb9a60ff7ae19c
Hamid Asadi <hamid.19471@gmail.com>
2024-06-24 21:05:18 +0330

    Update: add more style and user information

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,7 +2,7 @@ import { useAppContext } from "@/context/app-context/app-context";
 import { getUserResumes } from "@/core/HttpService";
 import CreateResume from "@/feature/create-resume/create-resume";
 import ResumeList from "@/feature/resume-list/resume-list";
-import { useUser } from "@clerk/clerk-react";
+import { UserButton, useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 
 const Dashboard = () => {
@@ -22,8 +22,15 @@ const Dashboard = () => {
   };
   return (
     <div className="container mt-12 ">
-      <h2 className="font-bold text-3xl ">My Resumes</h2>
-      <p className="font-semibold text-sm">View and edit your resumes.</p>
+      <div className="flex items-center justify-between bg-white/20 p-6 rounded-lg shadow-lg">
+        <div>
+          <h2 className="font-bold text-3xl ">My Resumes</h2>
+          <p className="font-semibold text-sm">Create or edit your resumes.</p>
+        </div>
+        <div>
+          <UserButton />
+        </div>
+      </div>
       <CreateResume />
       <div>
         <ResumeList resumeList={resumeList} />
